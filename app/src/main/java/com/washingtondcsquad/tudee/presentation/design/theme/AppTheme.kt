c725fe279b02ba6ec42e62752a4e913b25338da7
Sara Ashraf <saraashraf1822004@gmail.com>
2025-06-14 15:45:38 +0300

    refactor: update AppTheme

--- app/src/main/java/com/washingtondcsquad/tudee/presentation/design/theme/AppTheme.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/design/theme/AppTheme.kt
@@ -3,6 +3,7 @@ package com.washingtondcsquad.tudee.presentation.design.theme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.Stable
+import com.washingtondcsquad.tudee.presentation.design.colors.TudeeColors
 import com.washingtondcsquad.tudee.presentation.design.textStyle.TudeeTextStyle
 
 @Stable
@@ -12,6 +13,11 @@ object AppTheme {
         @Composable
         @ReadOnlyComposable
         get()= LocalTudeeTextStyle.current
+
+    val tudeeColors: TudeeColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTudeeColors.current
 }
 
 
