a47bec870fafb2c5314633448e674e332944d4bd
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-06-14 04:13:40 +0300

    Define TudeeTheme composable function
    This function provides the default text styles for the application.

---
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/design/theme/TudeeTheme.kt
@@ -0,0 +1,22 @@
+package com.washingtondcsquad.tudee.presentation.design.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import com.washingtondcsquad.tudee.presentation.design.textStyle.LocalTudeeTextStyle
+import com.washingtondcsquad.tudee.presentation.design.textStyle.defaultTextStyle
+
+@Composable
+fun TudeeTheme(
+    useDarkTheme: Boolean ,
+    content: @Composable () -> Unit
+){
+    if (useDarkTheme){ //todo check if dark theme is enabled
+    }
+    CompositionLocalProvider(
+        LocalTudeeTextStyle provides defaultTextStyle,
+    ) {
+        content()
+    }
+
+}
