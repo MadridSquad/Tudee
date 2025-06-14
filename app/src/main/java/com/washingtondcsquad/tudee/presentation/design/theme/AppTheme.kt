67aaca013a60168648a45742bb95c250c7f69455
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-06-14 14:37:59 +0300

    text-style-theme (#1)
    * Add `TudeeTextStyle` data class, which defines text styles for headlines, titles, bodies, and labels.
    
    * `SizedTextStyle` data class
    This data class represents a set of text styles with different sizes: large, medium, and small.
    
    * feat: Add nunito font family to the project resources.
    
    * adding a font family with normal, medium, and semi-bold weights.
    It also includes a local composition for the default text style.
    
    * Define `defaultTextStyle` for the Tudee app.
    
    * Refactor: Delete Typography.kt
    
    * Define TudeeTheme composable function
    
    This function provides the default text styles for the application.
    
    * add textStyle to
    The `AppTheme` now only provides access to `textStyle` via `LocalTudeeTextStyle`.
    
    * Modify MainActivity to utilize TudeeTheme
    
    * Create `themeLocals.kt` and move `LocalTudeeTextStyle` into it.
    
    This change aims to improve code organization by separating theme-related local compositions.
    The Android Gradle Plugin version has also been updated from 8.10.0 to 8.8.0.

---
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/design/theme/AppTheme.kt
@@ -0,0 +1,18 @@
+package com.washingtondcsquad.tudee.presentation.design.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.Stable
+import com.washingtondcsquad.tudee.presentation.design.textStyle.TudeeTextStyle
+
+@Stable
+object AppTheme {
+
+    val textStyle: TudeeTextStyle
+        @Composable
+        @ReadOnlyComposable
+        get()= LocalTudeeTextStyle.current
+}
+
+
+
---
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/design/theme/themeLocals.kt
@@ -0,0 +1,6 @@
+package com.washingtondcsquad.tudee.presentation.design.theme
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.washingtondcsquad.tudee.presentation.design.textStyle.defaultTextStyle
+
+val LocalTudeeTextStyle = staticCompositionLocalOf { defaultTextStyle }
