126481758476df38ec7944ee0b8dcf8b3e799d5d
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-06-14 04:11:10 +0300

    `SizedTextStyle` data class This data class represents a set of text styles with different sizes: large, medium, and small.

---
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/design/textStyle/SizedTextStyle.kt
@@ -0,0 +1,10 @@
+package com.washingtondcsquad.tudee.presentation.design.textStyle
+
+import androidx.compose.ui.text.TextStyle
+
+
+data class SizedTextStyle(
+    val large: TextStyle,
+    val medium: TextStyle,
+    val small: TextStyle,
+)
