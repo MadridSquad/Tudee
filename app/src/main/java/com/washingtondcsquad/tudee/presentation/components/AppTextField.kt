c23abd096951e322977ee0daad6d018777909f05
kareem esam <kareem.eldeen.esam@gmail.com>
2025-06-16 00:53:22 +0300

    feat: add shared UI components (#8)
    * feat: add shared UI components
    
    * feat: update TaskUiState to use String for taskId and add taskDate
    
    * feat: add calendar icon and display task date in TaskCard

---
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/components/AppTextField.kt
@@ -0,0 +1,113 @@
+package com.washingtondcsquad.tudee.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.washingtondcsquad.tudee.R
+import com.washingtondcsquad.tudee.presentation.design.theme.AppTheme
+
+@Composable
+fun AppTextField(
+    prefixIconPainter: Painter?,
+    hintText: String,
+    onValueChange: (String) -> Unit,
+    value: String,
+    modifier: Modifier = Modifier,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+) {
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    TextField(
+        interactionSource = interactionSource,
+        value = value,
+        textStyle = AppTheme.textStyle.label.medium,
+        onValueChange = onValueChange,
+        placeholder = {
+            Text(
+                text = hintText,
+                color = AppTheme.colors.stroke
+            )
+        },
+        shape = RoundedCornerShape(16.dp),
+        leadingIcon = if (prefixIconPainter != null) {
+            {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Icon(
+                        painter = prefixIconPainter,
+                        contentDescription = null,
+                        tint = AppTheme.colors.hint
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .width(1.dp)
+                            .height(32.dp)
+                            .background(AppTheme.colors.stroke)
+                    )
+                }
+            }
+        } else null,
+
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = Color.Transparent,
+            focusedContainerColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedTextColor = if (value.isEmpty()) AppTheme.colors.hint else AppTheme.colors.body,
+            focusedTextColor = AppTheme.colors.body,
+        ),
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = if (isFocused) AppTheme.colors.primary else AppTheme.colors.stroke,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(horizontal = 12.dp, vertical = 12.dp),
+    )
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    val text = remember { mutableStateOf("") }
+    AppTextField(
+        prefixIconPainter = painterResource(R.drawable.education_icon),
+        hintText = "Email",
+        onValueChange = {
+            text.value = it
+        },
+        value = text.value,
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    )
+}
