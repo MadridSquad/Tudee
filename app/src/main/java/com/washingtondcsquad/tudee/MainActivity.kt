c641875ee90f3b7cee2a3105c6891f799d270286
Sara Ashraf <125828072+saraashraf11@users.noreply.github.com>
2025-06-17 00:54:50 +0300

    #16-switch-button (#32)
    * refactor: add dependencies and delete empty files
    
    * add: vector and circle drawables
    
    * add: implement StarsOverlay composable
    
    * add: implement animated clouds overlay
    
    * add: implement LightModeIcon composable
    
    * add: implement DarkModeIcon composable
    
    * add: implement CustomSwitchButton

--- app/src/main/java/com/washingtondcsquad/tudee/MainActivity.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/MainActivity.kt
@@ -4,6 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.washingtondcsquad.tudee.presentation.components.CustomSwitchButton
 import com.washingtondcsquad.tudee.presentation.design.theme.TudeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,6 +24,22 @@ class MainActivity : ComponentActivity() {
             TudeeTheme(
                 useDarkTheme = false ,
             ) {
+
+                var isDark by remember { mutableStateOf(false) }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CustomSwitchButton(
+                        switchPadding = 2.dp,
+                        buttonWidth = 64.dp,
+                        buttonHeight = 36.dp,
+                        value = isDark,
+                        onToggle = { isDark = it }
+                    )
+                }
 
             }
         }
---
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/components/CloudsOverlay.kt
@@ -0,0 +1,93 @@
+package com.washingtondcsquad.tudee.presentation.components
+
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.washingtondcsquad.tudee.R
+
+@Composable
+fun CloudsOverlay(visible: Boolean) {
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        val offsetRight by animateDpAsState(
+            targetValue = if (visible) 0.dp else 40.dp,
+            animationSpec = tween(durationMillis = 800),
+            label = "offsetRight"
+        )
+
+        val offsetLeft by animateDpAsState(
+            targetValue = if (visible) 0.dp else (-40).dp,
+            animationSpec = tween(durationMillis = 800),
+            label = "offsetLeft"
+        )
+
+        val alphaAnim by animateFloatAsState(
+            targetValue = if (visible) 1f else 0f,
+            animationSpec = tween(durationMillis = 800),
+            label = "alphaAnim"
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.big_circle_gray),
+            contentDescription = "Big Circle gray",
+            modifier = Modifier
+                .width(32.dp)
+                .height(32.dp)
+                .align(Alignment.TopEnd)
+                .offset(x = 7.dp + offsetRight)
+                .clip(CircleShape)
+                .alpha(alphaAnim)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.big_circle_white),
+            contentDescription = "Big circle white",
+            modifier = Modifier
+                .size(29.dp)
+                .align(Alignment.TopEnd)
+                .offset(x = 7.dp + offsetLeft)
+                .alpha(alphaAnim)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.small_circle_gray),
+            contentDescription = "Small circle gray",
+            modifier = Modifier
+                .size(24.dp)
+                .align(Alignment.BottomEnd)
+                .offset(x = (-7).dp + offsetRight, y = 4.dp)
+                .alpha(alphaAnim)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.small_circle_white),
+            contentDescription = "Small circle white",
+            modifier = Modifier
+                .size(16.dp)
+                .align(Alignment.BottomEnd)
+                .offset(x = (-12).dp + offsetLeft, y = 4.dp)
+                .alpha(alphaAnim)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.small_circle_white),
+            contentDescription = "Small circle white",
+            modifier = Modifier
+                .size(16.dp)
+                .align(Alignment.BottomEnd)
+                .offset(x = offsetRight)
+                .alpha(alphaAnim)
+        )
+    }
+}
