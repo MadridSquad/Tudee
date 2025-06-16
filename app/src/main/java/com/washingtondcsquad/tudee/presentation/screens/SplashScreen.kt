db1c922c731ced85297320a6d35e5e2719e143a4
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-06-17 01:03:32 +0300

    add: add string resource for "Background graphics"

--- app/src/main/java/com/washingtondcsquad/tudee/presentation/screens/SplashScreen.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/screens/SplashScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.washingtondcsquad.tudee.R
 import com.washingtondcsquad.tudee.presentation.components.TextLogo
 import com.washingtondcsquad.tudee.presentation.design.AppTheme
@@ -27,7 +28,7 @@ fun SplashScreen(
            painter = if (isDarkTheme) painterResource(R.drawable.background_graphics_dark) else painterResource(
                R.drawable.background_graphics
            ),
-            contentDescription = "Background graphics",
+           contentDescription = stringResource(R.string.background_graphics),
             modifier = Modifier
                 .fillMaxSize()
                 .background(AppTheme.colors.overlay),
