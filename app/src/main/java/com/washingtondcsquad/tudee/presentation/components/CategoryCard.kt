49a101ce6c95ccd8c04c016f8f3f4b635f9d6b81
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-06-17 00:23:42 +0300

    Refactor: Update AppTheme import path

--- app/src/main/java/com/washingtondcsquad/tudee/presentation/components/CategoryCard.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/components/CategoryCard.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.washingtondcsquad.tudee.R
-import com.washingtondcsquad.tudee.presentation.design.theme.AppTheme
+import com.washingtondcsquad.tudee.presentation.design.AppTheme
 import com.washingtondcsquad.tudee.presentation.utils.modifierExensions.noRippleClick
 
 @Composable
--- app/src/main/java/com/washingtondcsquad/tudee/presentation/components/SnackBarCard.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/components/SnackBarCard.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.washingtondcsquad.tudee.R
-import com.washingtondcsquad.tudee.presentation.design.theme.AppTheme
+import com.washingtondcsquad.tudee.presentation.design.AppTheme
 import com.washingtondcsquad.tudee.presentation.utils.modifierExensions.dropShadow
 
 
