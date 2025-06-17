47432c73b4b8e87ab4d3e32762c4345e5c7afcf4
kareem esam <kareem.eldeen.esam@gmail.com>
2025-06-17 19:48:17 +0300

    feat: add koin core depndency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.koin.core)
     implementation(libs.koin.android)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
