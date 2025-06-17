6282a91d91c4c25947e71ce0dd4b882c0f031691
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2025-06-17 15:08:10 +0300

    feat: Implement Onboarding Screen

---
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/screen/onBoarding/OnBoardingPagerIndicator.kt
@@ -0,0 +1,65 @@
+package com.washingtondcsquad.tudee.presentation.screen.onBoarding
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.washingtondcsquad.tudee.presentation.design.theme.AppTheme
+import com.washingtondcsquad.tudee.presentation.design.theme.TudeeTheme
+
+@Composable
+fun OnBoardingPagerIndicator(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = modifier.wrapContentHeight()
+    ) {
+        repeat(pagerState.pageCount) { iteration ->
+            val targetColor =
+                if (pagerState.currentPage == iteration) AppTheme.colors.primary else AppTheme.colors.primaryVarient
+            val color by animateColorAsState(
+                targetValue = targetColor,
+                label = "PagerIndicatorColor"
+            )
+            Box(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(100.dp))
+                    .height(5.dp)
+                    .weight(1f)
+                    .background(color)
+
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun OnBoardingPagerIndicatorPrev() {
+    Box(contentAlignment = Alignment.Center) {
+        TudeeTheme(true) {
+            OnBoardingPagerIndicator(
+                pagerState = rememberPagerState(
+                    initialPage = 0,
+                    pageCount = { 3 }
+                )
+            )
+
+        }
+    }
+}
---
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/screen/onBoarding/OnBoardingScreen.kt
@@ -0,0 +1,116 @@
+package com.washingtondcsquad.tudee.presentation.screen.onBoarding
+
+import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.washingtondcsquad.tudee.R
+import com.washingtondcsquad.tudee.presentation.design.theme.AppTheme
+import com.washingtondcsquad.tudee.presentation.design.theme.TudeeTheme
+import kotlinx.coroutines.launch
+
+@Composable
+fun OnBoardingScreen(
+    modifier: Modifier = Modifier, onFinish: () -> Unit, viewModel: OnboardingViewModel
+) {
+    val state by viewModel.state.collectAsState()
+    val pagerState = rememberPagerState(
+        initialPage = state.currentPage, pageCount = {
+            state.pages.size
+        })
+    LaunchedEffect(pagerState.currentPage) {
+        viewModel.onPageChanged(pagerState.currentPage)
+    }
+    val scope = rememberCoroutineScope()
+
+    OnBoardingScreenContent(
+        modifier = modifier,
+        uiState = state,
+        onNextClick = {
+            if (state.isLastPage) {
+                onFinish()
+            } else {
+                scope.launch {
+                    pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                }
+            }
+        },
+        onSkipClick = {
+            scope.launch {
+                val lastPage = state.pages.size - 1
+                pagerState.animateScrollToPage(lastPage)
+            }
+        },
+        pagerState = pagerState
+    )
+}
+
+@SuppressLint("UnusedContentLambdaTargetStateParameter")
+@Composable
+fun OnBoardingScreenContent(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState,
+    uiState: OnboardingUiState,
+    onNextClick: () -> Unit,
+    onSkipClick: () -> Unit
+) {
+    Box(
+        modifier
+            .fillMaxSize()
+            .background(AppTheme.colors.overlay)
+            .statusBarsPadding()
+            .navigationBarsPadding()
+    ) {
+        AnimatedContent(pagerState.currentPage != pagerState.pageCount - 1) {
+            if (it) {
+                Text(
+                    text = "Skip",
+                    style = AppTheme.textStyle.label.large,
+                    color = AppTheme.colors.primary,
+                    modifier = Modifier
+                        .align(Alignment.TopStart)
+                        .padding(top = 16.dp, start = 16.dp)
+                        .clickable {
+                            onSkipClick()
+                        })
+
+            }
+        }
+
+        Column(
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            OnboardingPager(
+                modifier = Modifier, pagerState = pagerState, pages = uiState.pages,
+                onNextClick = { onNextClick() }
+            )
+            Spacer(Modifier.size(32.dp))
+            OnBoardingPagerIndicator(
+                modifier = Modifier.padding(start = 20.dp, end = 20.dp, bottom = 24.dp),
+                pagerState = pagerState,
+            )
+        }
+
+
+    }
+}
