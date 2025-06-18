cfffa7aa5e2c7c0bda1bb77cb90dc19e64ff060e
omer1998 <omerfaris11@gmail.com>
2025-06-18 04:21:03 +0300

    create the data associated with each navigation item

--- app/src/main/java/com/washingtondcsquad/tudee/presentation/components/bottom_nav_bar/NavBarItemData.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/components/bottom_nav_bar/NavBarItemData.kt
@@ -4,6 +4,7 @@ import androidx.annotation.DrawableRes
 import com.washingtondcsquad.tudee.R
 
 class NavBarItemData(
+    val route : String,
     val label: String,
     @DrawableRes val selectedIcon : Int,
     @DrawableRes val unSelectedIcon : Int
@@ -11,16 +12,19 @@ class NavBarItemData(
 
 var navBarItemsList = listOf(
     NavBarItemData(
+        route = "home",
         label = "Home",
         selectedIcon = R.drawable.home_icon_filled,
         unSelectedIcon = R.drawable.home_icon_outlined,
     ),
     NavBarItemData(
+        route = "task",
         label = "Task",
         selectedIcon = R.drawable.task_icon_filled,
         unSelectedIcon = R.drawable.task_icon_outlined,
     ),
     NavBarItemData(
+        route = "category",
         label = "Category",
         selectedIcon = R.drawable.category_icon_filled,
         unSelectedIcon = R.drawable.category_icon_outlined,
