46eba70b7df301fe66310044e9eb482d033f2b21
nada <nadamansoursamir@gmail.com>
2025-06-18 16:18:13 +0300

    the temporary code for testing the ConfirmDeleteTask composable has been removed.

--- app/src/main/java/com/washingtondcsquad/tudee/MainActivity.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/MainActivity.kt
@@ -5,17 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.washingtondcsquad.tudee.presentation.design.AppTheme
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.washingtondcsquad.tudee.presentation.deletetask.ConfirmDeleteTask
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,29 +14,6 @@ class MainActivity : ComponentActivity() {
             AppTheme(
                 useDarkTheme = false,
             ) {
-
-
-                var isSheet by rememberSaveable { mutableStateOf(false) }
-
-                Box {
-                    Button(
-                        onClick = {
-                            isSheet = true
-                        },
-                        modifier = Modifier.padding(16.dp)
-                    ) {
-                        Text("Open Bottom Sheet")
-                    }
-
-                    if (isSheet) {
-                        ConfirmDeleteTask(
-                            onDismiss = { isSheet = false },
-                            deleteOnClick = {},
-                            cancelOnClick = {}
-                        )
-                    }
-                }
-
             }
         }
     }
