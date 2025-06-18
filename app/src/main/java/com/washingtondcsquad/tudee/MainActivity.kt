364f4afabb1699a7d32cb4c108ae46cca266d082
omer1998 <omerfaris11@gmail.com>
2025-06-19 00:27:20 +0300

    create initial nav host/ graph structure

--- app/src/main/java/com/washingtondcsquad/tudee/MainActivity.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/MainActivity.kt
@@ -4,56 +4,56 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.BottomAppBarDefaults.windowInsets
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-
 import com.washingtondcsquad.tudee.presentation.components.bottom_nav_bar.TudeeNavigationBar
 import com.washingtondcsquad.tudee.presentation.components.bottom_nav_bar.navBarItemsList
+import com.washingtondcsquad.tudee.presentation.design.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
-            Scaffold(
-                bottomBar = {
-                    TudeeNavigationBar(
-                        navBarItemDataList = navBarItemsList,
+
+            AppTheme(
+                useDarkTheme = false
+            ) {
+
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        TudeeNavigationBar(
+                            navBarItemDataList = navBarItemsList,
+                            navController = navController,
+                            modifier = Modifier.windowInsetsPadding(windowInsets)
+                        )
+                    }) { innerPadding ->
+                    NavHost(
                         navController = navController,
-                        modifier = Modifier.windowInsetsPadding(windowInsets)
-                    )
-                }) { innerPadding ->
-                NavHost(
-                    navController = navController,
-                    startDestination = "home",
-                    modifier = Modifier.padding(innerPadding)
-                ) {
-                    composable(route = "home") {
-                        // home screen
-                    }
-                    composable(route = "task") {
-                        // tasks screen
-                    }
-                    composable(route = "category") {
-                        // Category screen
+                        startDestination = "home",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable(route = "home") {
+                            // home screen
+                        }
+                        composable(route = "task") {
+                            // tasks screen
+                        }
+                        composable(route = "category") {
+                            // Category screen
+                        }
                     }
                 }
             }
         }
     }
 }
+
 
