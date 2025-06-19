7e9c3fc071437580eedcbcfb2ef80556c8f1dd20
Mostafa Mohammed <tofymofy.55@gmail.com>
2025-06-19 08:39:58 +0300

    update task card to use the updated task priority card

--- app/src/main/java/com/washingtondcsquad/tudee/presentation/features/sharedUiState/TaskUiState.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/features/sharedUiState/TaskUiState.kt
@@ -1,10 +1,12 @@
 package com.washingtondcsquad.tudee.presentation.features.sharedUiState
 
+import com.washingtondcsquad.tudee.domain.entity.Priority
+
 data class TaskUiState(
     val taskId: String = "",
     val taskDate: String = "",
     val taskTitle: String = "",
     val taskDescription: String = "",
-    val taskPriority: String = "",
+    val taskPriority: Priority = Priority.LOW,
     val categoryImage: String = "",
 )
