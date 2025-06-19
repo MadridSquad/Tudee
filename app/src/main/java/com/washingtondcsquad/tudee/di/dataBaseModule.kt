4bcd11e5e6f0902ddf3f465817d48b0e18e5f161
Khaled.Eid <khaled.eid1k1k@gmail.com>
2025-06-19 16:43:36 +0200

    Remove: Delete ImageStorageManager dependency from dataBaseModule
    The `ImageStorageManager` dependency has been removed from the `dataBaseModule` in Koin. This indicates it is no longer being provided through this specific module.

--- app/src/main/java/com/washingtondcsquad/tudee/di/dataBaseModule.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/di/dataBaseModule.kt
@@ -1,14 +1,10 @@
 package com.washingtondcsquad.tudee.di
 
 import com.washingtondcsquad.tudee.data.localSource.TudeeDataBase
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
-import com.washingtondcsquad.tudee.data.localSource.ImageStorageManager
 
 
 val dataBaseModule =
     module {
         single { TudeeDataBase.getInstance(context = get()).daoCategory() }
-        singleOf(::ImageStorageManager)
     }
