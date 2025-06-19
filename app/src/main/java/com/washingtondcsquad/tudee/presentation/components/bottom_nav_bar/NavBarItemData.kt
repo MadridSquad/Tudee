5432a266ea4ca6cd9912bf98333bdf2eec89056c
omer-faris <43358116+omer1998@users.noreply.github.com>
2025-06-19 15:01:38 +0300

    Feature/#26 Bottom Navigation Bar  (#47)
    * adding navigation icon
    
    * add bottom nav bar components
    
    * add navigaton dependencies
    
    * create the data associated with each navigation item
    
    * create the custom navigation bar
    
    * experiment with bottom navbar  after implementing navigation
    
    * refactor nav host
    
    * clean imports issues
    
    * create initial nav host/ graph structure
    
    * handle not showing bottom nav bar on certain routes
    
    * organize dependencies
    
    * fix animation
    
    * clean the file
    
    * handle hiding bottom nav bar on specific route smoothly

---
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/components/bottom_nav_bar/NavBarItemData.kt
@@ -0,0 +1,34 @@
+package com.washingtondcsquad.tudee.presentation.components.bottom_nav_bar
+
+import androidx.annotation.DrawableRes
+import com.washingtondcsquad.tudee.R
+
+class NavBarItemData(
+    val route: String,
+    val label: String,
+    @DrawableRes val selectedIcon: Int,
+    @DrawableRes val unSelectedIcon: Int
+)
+
+var navBarItemsList = listOf(
+    NavBarItemData(
+        route = "home",
+        label = "Home",
+        selectedIcon = R.drawable.home_icon_filled,
+        unSelectedIcon = R.drawable.home_icon_outlined,
+    ), NavBarItemData(
+        route = "task",
+        label = "Task",
+        selectedIcon = R.drawable.task_icon_filled,
+        unSelectedIcon = R.drawable.task_icon_outlined,
+    ), NavBarItemData(
+        route = "category",
+        label = "Category",
+        selectedIcon = R.drawable.category_icon_filled,
+        unSelectedIcon = R.drawable.category_icon_outlined,
+    )
+)
+
+val bottomNavBarRoutes = navBarItemsList.map {
+    it.route
+}
