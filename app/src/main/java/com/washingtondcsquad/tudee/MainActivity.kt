523cb2bdff58cf45d4fffa517a398ee1ec7bfb9f
Mostafa Mohamed <41519636+MostafaMohamed2002@users.noreply.github.com>
2025-06-20 00:47:26 +0300

    Feat: Integrate Onboarding Screen (#59)
    * Inject TaskRepository into OnboardingViewModel
    
    The `OnboardingViewModel` now receives `TaskRepository` as a dependency via constructor injection. This change is reflected in the Koin `viewModelModule`.
    
    * Remove status and navigation bar padding from OnBoardingScreen
    
    The `OnBoardingScreen` now uses `koinViewModel()` to obtain the `OnboardingViewModel` by default.
    
    * Integrate OnboardingScreen into MainActivity
    
    The `MainActivity` now displays an `OnBoardingScreen` if the user has not seen it before.
    The start destination of the `NavHost` is determined by the `isOnboardingShown` state, which is collected from `AppPreferencesService`.
    After completing the onboarding process, the app navigates to the "home" screen and removes "onboarding" from the back stack.
    The `HomeScreen` composable has also been added to the "home" route.
    An `@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")` has been added to suppress a lint warning.
    
    * Refactor: Defer pre-defined category creation until onboarding is confirmed
    
    This commit modifies the `MainActivity` to defer the creation of pre-defined categories until it's confirmed that the onboarding screen needs to be shown.
    
    Previously, `createPreDefineCategories()` was called unconditionally in `onCreate()`. Now, it's only invoked when the `isOnboardingShownState` indicates that onboarding has not been completed.
    
    Additionally, a loading indicator (`CircularProgressIndicator`) is displayed while the `isOnboardingShownState` is being determined to provide visual feedback to the user. The "category" route now navigates to `CategoriesScreen()`.
    
    ---------
    
    Co-authored-by: Khaled.Eid <khaled.eid1k1k@gmail.com>

--- app/src/main/java/com/washingtondcsquad/tudee/MainActivity.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.washingtondcsquad.tudee
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.BottomAppBarDefaults.windowInsets
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -31,8 +36,11 @@ import com.washingtondcsquad.tudee.presentation.features.home.HomeScreen
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import com.washingtondcsquad.tudee.presentation.features.home.HomeScreen
+import com.washingtondcsquad.tudee.presentation.screen.onBoarding.OnBoardingScreen
 import org.koin.android.ext.android.inject
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity() {
     private val appPreferencesService: AppPreferencesService by inject()
 
@@ -40,58 +48,77 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
-        createPreDefineCategories()
+
         setContent {
-            AppTheme {
-                CategoriesScreen(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(top = 48.dp)
-                )
-                val isDarkMode by appPreferencesService.isDarkModeEnabled()
-                    .collectAsState(initial = false)
+            val isDarkMode by appPreferencesService.isDarkModeEnabled()
+                .collectAsState(initial = false)
+            val isOnboardingShownFlow = appPreferencesService.hasOnboardingBeenShown()
+            val isOnboardingShownState by isOnboardingShownFlow.collectAsState(initial = null)
 
-                AppTheme(
-                    useDarkTheme = isDarkMode
-                ) {
+            AppTheme(
+                useDarkTheme = isDarkMode
+            ) {
+                when (val isOnboardingShown = isOnboardingShownState) {
+                    null -> {
+                        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                            CircularProgressIndicator()
+                        }
+                    }
+                    else -> {
+                        val startDestination = if (isOnboardingShown) {
+                            "home"
+                        } else {
+                            Log.d("sdasdsad", "onCreate: ")
+                            createPreDefineCategories()
+                            "onboarding"
+                        }
+                        val navController = rememberNavController()
+                        val navBackStackEntry = navController.currentBackStackEntryAsState()
+                        val currentDestination = navBackStackEntry.value?.destination?.route
 
-                    val navController = rememberNavController()
-                    val navBackStackEntry = navController.currentBackStackEntryAsState()
-                    val currentDestination = navBackStackEntry.value?.destination?.route
-                    Scaffold(
-                        bottomBar = {
-                            AnimatedVisibility(currentDestination in bottomNavBarRoutes) {
-                                TudeeNavigationBar(
-                                    navBarItemDataList = navBarItemsList,
-                                    navController = navController,
-                                    modifier = Modifier.windowInsetsPadding(windowInsets)
-                                )
-                            }
+                        Scaffold(
+                            bottomBar = {
+                                AnimatedVisibility(currentDestination in bottomNavBarRoutes) {
+                                    TudeeNavigationBar(
+                                        navBarItemDataList = navBarItemsList,
+                                        navController = navController,
+                                        modifier = Modifier.windowInsetsPadding(windowInsets)
+                                    )
+                                }
 
-                        }) { innerPadding ->
-                        NavHost(
-                            navController = navController,
-                            startDestination = "home",
-                            modifier = Modifier.padding(innerPadding)
-                        ) {
-                            composable(route = "home") {
-                               HomeScreen()
-                            }
-                            composable(route = "task") {
-
-
-                            }
-                            composable(route = "category") {
-                                CategoriesScreen()
+                            }) { innerPadding ->
+                            NavHost(
+                                navController = navController,
+                                startDestination = startDestination,
+                                modifier = Modifier.padding(innerPadding)
+                            ) {
+                                composable(route = "onboarding") {
+                                    OnBoardingScreen(
+                                        onFinish = {
+                                            navController.navigate("home") {
+                                                popUpTo("onboarding") { inclusive = true }
+                                            }
+                                        },
+                                    )
+                                }
+                                composable(route = "home") {
+                                    HomeScreen()
+                                }
+                                composable(route = "task") {
+                                    // tasks screen
+                                }
+                                composable(route = "category") {
+                                    CategoriesScreen()
+                                }
                             }
                         }
                     }
-
                 }
             }
-        }
 
-    }
+
+            }
+        }
 
     private fun createPreDefineCategories() {
         CoroutineScope(Dispatchers.IO).launch {
