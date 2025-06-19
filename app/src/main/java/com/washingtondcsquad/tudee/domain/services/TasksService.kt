8b427734332a15ae412964a24d2651b573a44e3c
kareem esam <kareem.eldeen.esam@gmail.com>
2025-06-19 06:16:54 +0300

    feat: update TasksService to use Int for task identifiers and integrate local data source

--- app/src/main/java/com/washingtondcsquad/tudee/domain/services/TasksService.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/domain/services/TasksService.kt
@@ -1,12 +1,11 @@
 package com.washingtondcsquad.tudee.domain.services
 
 import com.washingtondcsquad.tudee.domain.entity.Task
-import java.util.UUID
 
 interface TasksService {
     suspend fun createTask(task: Task)
-    suspend fun deleteTask(id: UUID)
+    suspend fun deleteTask(id: Int)
     suspend fun getAllTasks(): List<Task>
-    suspend fun getTaskById(id: UUID): Task
+    suspend fun getTaskById(id: Int): Task
     suspend fun editTask(task: Task)
 }
