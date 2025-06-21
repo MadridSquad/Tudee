a7f9f0bae2c4c07a88119065ec61d371cd478b8e
HendSayed25 <hendsayed853@gmail.com>
2025-06-21 15:51:57 +0300

    refactor entities in domain

--- app/src/main/java/com/washingtondcsquad/tudee/domain/entity/Category.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/domain/entity/Category.kt
@@ -1,9 +1,14 @@
 package com.washingtondcsquad.tudee.domain.entity
 
+import com.washingtondcsquad.tudee.presentation.features.sharedUiState.ImageSource
 
+
+@JvmInline
+value class CategoryID(val categoryId:Long)
 data class Category(
-    val id: Long,
+    val id: CategoryID,
     val title: String,
-    var iconPath: String,
-    val taskCount: Int,
+    var iconPath: ImageSource,  //TODO move imageSource class from presentation to domain
+    val taskCount: Int, // delete it from here or not ?
+    val isPredefined: Boolean //TODO new task
 )
