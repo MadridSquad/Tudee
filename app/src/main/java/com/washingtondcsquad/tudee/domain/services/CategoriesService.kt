fc8f6af0e9876b7345c2d811a170c061f71e192b
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-06-22 23:54:44 +0300

    added 2 new functions

--- app/src/main/java/com/washingtondcsquad/tudee/domain/services/CategoriesService.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/domain/services/CategoriesService.kt
@@ -2,18 +2,26 @@ package com.washingtondcsquad.tudee.domain.services
 
 import com.washingtondcsquad.tudee.domain.entity.Category
 import com.washingtondcsquad.tudee.domain.entity.CategoryID
-import com.washingtondcsquad.tudee.presentation.features.sharedUiState.ImageSource
+import com.washingtondcsquad.tudee.domain.entity.ImageCategory
+import com.washingtondcsquad.tudee.domain.entity.Task
 import kotlinx.coroutines.flow.Flow
 
 interface CategoriesService {
     suspend fun createCategory(
         title: String,
-        iconPath: ImageSource,
+        iconPath: ImageCategory,
         isPredefined: Boolean,
-        taskCounts: Int
     )
-    suspend fun deleteCategory(category: Category) //TODO write query to delete using id rather than send the whole category
+
+    suspend fun deleteCategory(category: CategoryID)
+
     suspend fun editCategory(category: Category)
+
     fun getAllCategories(): Flow<List<Category>>
+
     suspend fun getCategoryById(categoryId: CategoryID): Category
+
+    suspend fun getTaskCountByCategoryID(categoryId: CategoryID): Int
+
+    suspend fun getTasksByCategoryID(categoryId: CategoryID): List<Task>
 }
