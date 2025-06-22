15ee56a3b48b30fdecd81481646b2b0f46f3d0ce
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-06-22 18:49:10 +0300

    refactor: refactor inject of add task view model

--- app/src/main/java/com/washingtondcsquad/tudee/di/viewModelModule.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/di/viewModelModule.kt
@@ -1,15 +1,14 @@
 package com.washingtondcsquad.tudee.di
 
+import com.washingtondcsquad.tudee.presentation.features.add_task.AddTaskViewModel
 import com.washingtondcsquad.tudee.presentation.features.home.HomeViewModel
 import com.washingtondcsquad.tudee.presentation.features.task_details.BottomSheetTaskViewModel
 import com.washingtondcsquad.tudee.presentation.features.onBoarding.OnboardingViewModel
-import com.washingtondcsquad.tudee.presentation.features.add_task.AddTaskViewModel
 import com.washingtondcsquad.tudee.presentation.screens.add_task.EditTaskViewModel
 import com.washingtondcsquad.tudee.presentation.features.tasks_screen.TasksViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
-import java.time.LocalDate
 import com.washingtondcsquad.tudee.presentation.features.categories.CategoriesViewModel
 
 
@@ -22,13 +21,11 @@ val viewModelModule = module {
         get()
     ) }
     viewModel { BottomSheetTaskViewModel(get()) }
-    viewModel { (taskDate: LocalDate, onCancel: () -> Unit, onActionResult: (success: Boolean, message: String) -> Unit) ->
+    viewModel { (taskDate: kotlinx.datetime.LocalDate) ->
         AddTaskViewModel(
             tasksService = get(),
             categoryService = get(),
             taskDate = taskDate,
-            onCancelAddTaskBottomSheet = onCancel,
-            onActionResult = onActionResult
         )
     }
 
