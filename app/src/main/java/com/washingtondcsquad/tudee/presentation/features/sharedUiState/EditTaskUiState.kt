019ced1ec5df6b080c1bf6983552db9a09d459a8
Mohamed ElMokhtar <boumarafmohamed.co@gmail.com>
2025-06-23 10:17:20 -0700

    refactor: `EditTaskUiState` to use `kotlinx.datetime.LocalDate` for `taskDate`

--- app/src/main/java/com/washingtondcsquad/tudee/presentation/features/sharedUiState/EditTaskUiState.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/features/sharedUiState/EditTaskUiState.kt
@@ -2,12 +2,17 @@ package com.washingtondcsquad.tudee.presentation.features.sharedUiState
 
 import com.washingtondcsquad.tudee.domain.entity.Category
 import com.washingtondcsquad.tudee.domain.entity.Priority
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 data class EditTaskUiState(
     val taskId: Int = 0,
     val taskTitle: String  = "",
     val taskDescription: String = "",
-    val taskDate: String = "",
+    val taskDate: LocalDate = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date,
+
     val selectedPriority: Priority?= null,
     val selectedCategory: Category ?= null,
 
