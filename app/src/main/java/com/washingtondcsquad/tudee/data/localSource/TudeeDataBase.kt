b11c2ed5453d2308f1e78b8609749e1549ac3baf
HendSayed25 <hendsayed853@gmail.com>
2025-06-24 13:40:37 +0300

    add Converters for Database

--- app/src/main/java/com/washingtondcsquad/tudee/data/localSource/TudeeDataBase.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/data/localSource/TudeeDataBase.kt
@@ -4,12 +4,16 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import com.washingtondcsquad.tudee.data.localSource.daos.CategoryDao
 import com.washingtondcsquad.tudee.data.localSource.daos.TaskDao
 import com.washingtondcsquad.tudee.data.localSource.model.CategoryEntity
 import com.washingtondcsquad.tudee.data.localSource.model.TaskEntity
+import com.washingtondcsquad.tudee.data.localSource.utils.Converter
 
 @Database(entities = [CategoryEntity::class, TaskEntity::class], version = 1, exportSchema = false)
+@TypeConverters(Converter::class)
 abstract class TudeeDataBase : RoomDatabase() {
     abstract fun daoCategory(): CategoryDao
     abstract fun daoTask(): TaskDao
