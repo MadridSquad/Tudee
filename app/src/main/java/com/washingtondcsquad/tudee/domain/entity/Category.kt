90b46e9b34e78171371d2805af949fd300f172f8
HendSayed25 <hendsayed853@gmail.com>
2025-06-24 13:42:33 +0300

    remove task count and rename sealed class

--- app/src/main/java/com/washingtondcsquad/tudee/domain/entity/Category.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/domain/entity/Category.kt
@@ -6,11 +6,11 @@ value class CategoryID(val categoryId: Long)
 data class Category(
     val id: CategoryID,
     val title: String,
-    var iconPath: ImageCategory,  //TODO move imageSource class from presentation to domain
+    var iconPath: ImageSource,
     val isPredefined: Boolean, //TODO new task
 )
 
-sealed interface ImageCategory {
-    data class AddedByUser(val value: String) : ImageCategory
-    data class PredefinedDrawable(val id: Int) : ImageCategory
+sealed interface ImageSource {
+    data class AddedByUser(val value: String) : ImageSource
+    data class PredefinedDrawable(val id: Int) : ImageSource
 }
