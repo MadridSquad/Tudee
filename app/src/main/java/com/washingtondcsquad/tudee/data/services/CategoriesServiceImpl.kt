ef5215e96a1908c9481c8fb297986fa7a0ea1e6d
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-06-24 19:02:16 +0300

    refactor : categoryServiceImp

--- app/src/main/java/com/washingtondcsquad/tudee/data/services/CategoriesServiceImpl.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/data/services/CategoriesServiceImpl.kt
@@ -1,96 +1,83 @@
 package com.washingtondcsquad.tudee.data.services
 
 import android.util.Log
+import com.washingtondcsquad.tudee.R
 import com.washingtondcsquad.tudee.data.localSource.daos.CategoryDao
+import com.washingtondcsquad.tudee.data.localSource.daos.TaskDao
 import com.washingtondcsquad.tudee.data.localSource.mapper.toDomain
 import com.washingtondcsquad.tudee.data.localSource.mapper.toEntity
-import com.washingtondcsquad.tudee.domain.entity.Category
-import com.washingtondcsquad.tudee.domain.services.CategoriesService
-import com.washingtondcsquad.tudee.domain.services.ImageSaverService
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import com.washingtondcsquad.tudee.R
-import com.washingtondcsquad.tudee.data.localSource.daos.TaskDao
 import com.washingtondcsquad.tudee.data.localSource.model.CategoryEntity
+import com.washingtondcsquad.tudee.domain.entity.Category
 import com.washingtondcsquad.tudee.domain.entity.CategoryID
 import com.washingtondcsquad.tudee.domain.entity.ImageSource
 import com.washingtondcsquad.tudee.domain.entity.Task
 import com.washingtondcsquad.tudee.domain.provider.StringProvider
-import kotlinx.coroutines.flow.flow
+import com.washingtondcsquad.tudee.domain.services.CategoriesService
+import com.washingtondcsquad.tudee.domain.services.ImageSaverService
+import kotlinx.coroutines.flow.map
 
 class CategoriesServiceImpl(
     private val stringProvider: StringProvider,
     private val categoryDao: CategoryDao,
     private val imageSaver: ImageSaverService,
-    private val taskDao : TaskDao
-
-    ) : CategoriesService {
+    private val taskDao: TaskDao,
+) : CategoriesService {
 
     override suspend fun createCategory(
         title: String,
         iconPath: String,
-        isPredefined: Boolean
+        isPredefined: Boolean,
     ) {
-        val category = Category(
+        val category = CategoryEntity(
             title = title,
             id = CategoryID(0L), //TODO search how not send id because it's auto generation ???
-            iconPath = ImageSource.AddedByUser(saveCategoryImage(iconPath)),
+            image = saveCategoryImage(iconPath),
             isPredefined = false,
         )
-        categoryDao.createCategory(category.toEntity())
-        Log.e("save category","done")
+        categoryDao.createCategory(category)
+        Log.e("save category", "done")
     }
+
+    override suspend fun saveCategoryImage(iconPath: String): String {
+        var savedUrl = ""
+        imageSaver.saveImage(iconPath) { path ->
+            savedUrl = path
+        }
+        return savedUrl
+    }
+
 
     override suspend fun deleteCategory(categoryId: CategoryID) { //TODO delete image in presentation layer
         categoryDao.deleteCategory(categoryId)
     }
+
+    override suspend fun deleteCategoryImage(iconPath: ImageSource) {
+        when (val icon = iconPath) {
+            is ImageSource.AddedByUser -> {
+                imageSaver.deleteImage(icon.value)
+            }
+
+            is ImageSource.PredefinedDrawable -> TODO()
+        }
+    }
+
 
     override suspend fun editCategory(category: Category) {
         deleteCategoryImage(category.iconPath)
         categoryDao.editCategory(category.toEntity())
     }
 
-    override fun getAllCategories(): Flow<List<Category>> {
-        return flow {
-            emit(
-                  listOf(
-                      Category(
-                          id = CategoryID(0L),
-                          title = stringProvider.getString(R.string.education),
-                          isPredefined = true,
-                          iconPath = ImageSource.PredefinedDrawable(R.drawable.education_icon)
-                      )
-                  )
+    override fun getAllCategories() =
+        categoryDao.getAllCategories().map { flow -> flow.map { it.toDomain() } }
 
-            )
-        }
-        //categoryDao.getAllCategories().map { flow -> flow.map { it.toDomain() } }
-    }
+    override suspend fun getTaskCountByCategoryID(categoryId: CategoryID) =
+        taskDao.getTaskCountByCategoryId(categoryId)
 
-    override suspend fun getTaskCountByCategoryID(categoryId: CategoryID): Int {
-        return taskDao.getTaskCountByCategoryId(categoryId)
-    }
 
     override suspend fun getTasksByCategoryID(categoryId: CategoryID): List<Task> {
         return taskDao.getTasksByCategoryId(categoryId)
     }
 
-    override suspend fun saveCategoryImage(iconPath: String): String {
-        var savedUrl = ""
-        imageSaver.saveImage(iconPath) { path->
-            savedUrl = path
-       }
-        return savedUrl
-    }
-
-    override suspend fun deleteCategoryImage(iconPath: ImageSource) {
-        when(val icon = iconPath){
-            is ImageSource.AddedByUser ->{
-                imageSaver.deleteImage(icon.value)
-            }
-            is ImageSource.PredefinedDrawable -> TODO()
-        }
-    }
 
     override suspend fun getCategoryById(categoryId: CategoryID): Category {
         return categoryDao.getCategoryById(categoryId).toDomain()
@@ -100,112 +87,112 @@ class CategoriesServiceImpl(
         val categories = preparePredefinedCategories(stringProvider)
         categoryDao.createPredefinedCategories(categories)
     }
-}
 
-private fun preparePredefinedCategories(stringProvider: StringProvider): List<CategoryEntity>{
-    val categories = listOf(
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.education),
-            image = R.drawable.education_icon.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.shopping),
-            image = R.drawable.shopping.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.medical),
-            image = R.drawable.medical.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.gym),
-            image = R.drawable.gym.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.entertainment),
-            image = R.drawable.entertainment.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.cooking),
-            image = R.drawable.cooking.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.family),
-            image = R.drawable.family.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.traveling),
-            image = R.drawable.traveling.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.agriculture),
-            image = R.drawable.agriculture.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.coding),
-            image = R.drawable.coding.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.adoration),
-            image = R.drawable.adoration.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.fix_bug),
-            image = R.drawable.fix_bug.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.cleaning),
-            image = R.drawable.cleaning.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.work),
-            image = R.drawable.work.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.budgeting),
-            image = R.drawable.budgeting.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.self_care),
-            image = R.drawable.self_care.toString(),
-            isPredefined = true
-        ),
-        CategoryEntity(
-            id = CategoryID(0L),
-            title = stringProvider.getString(R.string.event),
-            image = R.drawable.event.toString(),
-            isPredefined = true
+    private fun preparePredefinedCategories(stringProvider: StringProvider): List<CategoryEntity> {
+        val categories = listOf(
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.education),
+                image = R.drawable.education_icon.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.shopping),
+                image = R.drawable.shopping.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.medical),
+                image = R.drawable.medical.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.gym),
+                image = R.drawable.gym.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.entertainment),
+                image = R.drawable.entertainment.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.cooking),
+                image = R.drawable.cooking.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.family),
+                image = R.drawable.family.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.traveling),
+                image = R.drawable.traveling.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.agriculture),
+                image = R.drawable.agriculture.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.coding),
+                image = R.drawable.coding.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.adoration),
+                image = R.drawable.adoration.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.fix_bug),
+                image = R.drawable.fix_bug.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.cleaning),
+                image = R.drawable.cleaning.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.work),
+                image = R.drawable.work.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.budgeting),
+                image = R.drawable.budgeting.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.self_care),
+                image = R.drawable.self_care.toString(),
+                isPredefined = true
+            ),
+            CategoryEntity(
+                id = CategoryID(0L),
+                title = stringProvider.getString(R.string.event),
+                image = R.drawable.event.toString(),
+                isPredefined = true
+            )
         )
-    )
-    return categories
+        return categories
+    }
 }
