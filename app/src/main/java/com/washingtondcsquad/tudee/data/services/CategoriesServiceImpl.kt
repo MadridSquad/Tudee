091ecad836ad79d192cd2dff3261a3a42476209c
HendSayed25 <hendsayed853@gmail.com>
2025-06-24 13:39:24 +0300

    implement methods

--- app/src/main/java/com/washingtondcsquad/tudee/data/services/CategoriesServiceImpl.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/data/services/CategoriesServiceImpl.kt
@@ -1,101 +1,195 @@
 package com.washingtondcsquad.tudee.data.services
 
-
-import android.content.Context
 import com.washingtondcsquad.tudee.data.localSource.daos.CategoryDao
-import com.washingtondcsquad.tudee.data.localSource.imageStorageManager.DeleteImageFromInternalStorage
-import com.washingtondcsquad.tudee.data.localSource.imageStorageManager.SaveImageToInternalStorage
 import com.washingtondcsquad.tudee.data.localSource.mapper.toDomain
 import com.washingtondcsquad.tudee.data.localSource.mapper.toEntity
 import com.washingtondcsquad.tudee.domain.entity.Category
 import com.washingtondcsquad.tudee.domain.services.CategoriesService
 import com.washingtondcsquad.tudee.domain.services.ImageSaverService
-import com.washingtondcsquad.tudee.presentation.features.sharedUiState.ImageSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import org.koin.compose.getKoin
 import com.washingtondcsquad.tudee.R
+import com.washingtondcsquad.tudee.data.localSource.daos.TaskDao
 import com.washingtondcsquad.tudee.data.localSource.model.CategoryEntity
+import com.washingtondcsquad.tudee.domain.entity.CategoryID
+import com.washingtondcsquad.tudee.domain.entity.ImageSource
+import com.washingtondcsquad.tudee.domain.entity.Task
+import com.washingtondcsquad.tudee.domain.provider.StringProvider
 
 class CategoriesServiceImpl(
-    private val stringProvider: StringProvider
-    private val daoCategory: CategoryDao,
+    private val stringProvider: StringProvider,
+    private val categoryDao: CategoryDao,
     private val imageSaver: ImageSaverService,
+    private val taskDao : TaskDao
 
     ) : CategoriesService {
 
     override suspend fun createCategory(
-        title: String, iconPath: ImageSource, isPredefined: Boolean, taskCounts: Int
+        title: String,
+        iconPath: String,
+        isPredefined: Boolean
     ) {
         val category = Category(
             title = title,
-            id = CategoryID(0L),
-            iconPath = iconPath,
-            taskCount = 0,
-            isPredefined = true,
+            id = CategoryID(0L), //TODO search how not send id because it's auto generation ???
+            iconPath = ImageSource.AddedByUser(saveCategoryImage(iconPath)),
+            isPredefined = false,
         )
-        imageSaver.saveImage(category.iconPath) {
-            category.iconPath = ImageSource.Path(it) //TODO check it
-        }
-        daoCategory.createCategory(category.toEntity())
-
+        categoryDao.createCategory(category.toEntity())
     }
 
-    override suspend fun deleteCategory(category: Category) {
-        imageSaver.deleteImage(category.iconPath)
-        daoCategory.deleteCategory(category.toEntity())
+    override suspend fun deleteCategory(categoryId: CategoryID) { //TODO delete image in presentation layer
+        categoryDao.deleteCategory(categoryId)
     }
 
     override suspend fun editCategory(category: Category) {
-        imageSaver.deleteImage(category.iconPath)
-        daoCategory.editCategory(category.toEntity())
+        deleteCategoryImage(category.iconPath)
+        categoryDao.editCategory(category.toEntity())
     }
 
     override fun getAllCategories(): Flow<List<Category>> {
-        return daoCategory.getAllCategories().map { flow -> flow.map { it.toDomain() } }
+        return categoryDao.getAllCategories().map { flow -> flow.map { it.toDomain() } }
     }
 
     override suspend fun getTaskCountByCategoryID(categoryId: CategoryID): Int {
-        TODO("Not yet implemented")
+        return taskDao.getTaskCountByCategoryId(categoryId)
     }
 
     override suspend fun getTasksByCategoryID(categoryId: CategoryID): List<Task> {
-        TODO("Not yet implemented")
+        return taskDao.getTasksByCategoryId(categoryId)
+    }
+
+    override suspend fun saveCategoryImage(iconPath: String): String {
+        var savedUrl = ""
+        imageSaver.saveImage(iconPath) { path->
+            savedUrl = path
+       }
+        return savedUrl
+    }
+
+    override suspend fun deleteCategoryImage(iconPath: ImageSource) {
+        when(val icon = iconPath){
+            is ImageSource.AddedByUser ->{
+                imageSaver.deleteImage(icon.value)
+            }
+            is ImageSource.PredefinedDrawable -> TODO()
+        }
+    }
+
+    override suspend fun getCategoryById(categoryId: CategoryID): Category {
+        return categoryDao.getCategoryById(categoryId).toDomain()
     }
 
     override suspend fun createPreDefinedCategories() {
-        val titles = listOf(
-            CategoryEntity(
-                CategoryID(0L),
-                stringProvider.getString(R.string.education),
-                image = R.drawable.education_icon.toString(),
-                isPredefined = true
-            )
+        val categories = preparePredefinedCategories(stringProvider)
+        categoryDao.createPredefinedCategories(categories)
+    }
+}
 
-                    stringProvider . getString (R.string.shopping),
-            stringProvider.getString(R.string.medical),
-            stringProvider.getString(R.string.gym),
-            stringProvider.getString(R.string.entertainment),
-            stringProvider.getString(R.string.cooking),
-            stringProvider.getString(R.string.family),
-            stringProvider.getString(R.string.traveling),
-            stringProvider.getString(R.string.agriculture),
-            stringProvider.getString(R.string.coding),
-            stringProvider.getString(R.string.adoration),
-            stringProvider.getString(R.string.fix_bug),
-            stringProvider.getString(R.string.cleaning),
-            stringProvider.getString(R.string.work),
-            stringProvider.getString(R.string.budgeting),
-            stringProvider.getString(R.string.self_care),
-            stringProvider.getString(R.string.event)
+private fun preparePredefinedCategories(stringProvider: StringProvider): List<CategoryEntity>{
+    val categories = listOf(
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.education),
+            image = R.drawable.education_icon.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.shopping),
+            image = R.drawable.shopping.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.medical),
+            image = R.drawable.medical.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.gym),
+            image = R.drawable.gym.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.entertainment),
+            image = R.drawable.entertainment.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.cooking),
+            image = R.drawable.cooking.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.family),
+            image = R.drawable.family.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.traveling),
+            image = R.drawable.traveling.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.agriculture),
+            image = R.drawable.agriculture.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.coding),
+            image = R.drawable.coding.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.adoration),
+            image = R.drawable.adoration.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.fix_bug),
+            image = R.drawable.fix_bug.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.cleaning),
+            image = R.drawable.cleaning.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.work),
+            image = R.drawable.work.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.budgeting),
+            image = R.drawable.budgeting.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.self_care),
+            image = R.drawable.self_care.toString(),
+            isPredefined = true
+        ),
+        CategoryEntity(
+            id = CategoryID(0L),
+            title = stringProvider.getString(R.string.event),
+            image = R.drawable.event.toString(),
+            isPredefined = true
         )
-
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getCategoryById(categoryId: CategoryID): Category {
-        return daoCategory.getCategoryById(categoryIKd).toDomain()
-    override suspend fun getCategoryById(categoryId: CategoryID): Category {
-        return daoCategory.getCategoryById(categoryId).toDomain()
-    }
+    )
+    return categories
 }
--- app/src/main/java/com/washingtondcsquad/tudee/domain/services/CategoriesService.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/domain/services/CategoriesService.kt
@@ -2,29 +2,23 @@ package com.washingtondcsquad.tudee.domain.services
 
 import com.washingtondcsquad.tudee.domain.entity.Category
 import com.washingtondcsquad.tudee.domain.entity.CategoryID
-import com.washingtondcsquad.tudee.domain.entity.ImageCategory
+import com.washingtondcsquad.tudee.domain.entity.ImageSource
 import com.washingtondcsquad.tudee.domain.entity.Task
 import kotlinx.coroutines.flow.Flow
 
 interface CategoriesService {
     suspend fun createCategory(
         title: String,
-        iconPath: ImageCategory,
+        iconPath: String,
         isPredefined: Boolean,
     )
-
     suspend fun deleteCategory(category: CategoryID)
-
     suspend fun editCategory(category: Category)
-
     fun getAllCategories(): Flow<List<Category>>
-
     suspend fun getCategoryById(categoryId: CategoryID): Category
-
     suspend fun getTaskCountByCategoryID(categoryId: CategoryID): Int
-
     suspend fun getTasksByCategoryID(categoryId: CategoryID): List<Task>
-
     suspend fun createPreDefinedCategories()
-
+    suspend fun saveCategoryImage(iconPath: String): String
+    suspend fun deleteCategoryImage(iconPath: ImageSource)
 }
