962429bf2a55d17a76ac04031455f512b1657538
karrar-abbas <karar.abbas.ch@gmail.com>
2025-06-25 18:13:00 +0300

    refactor: make delete fun invoke when scrolling task card

--- app/src/main/java/com/washingtondcsquad/tudee/presentation/features/delete_task/DeleteTask.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/features/delete_task/DeleteTask.kt
@@ -6,12 +6,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,29 +35,22 @@ import kotlin.math.roundToInt
 @Composable
 fun DeleteTaskBackground(
     modifier: Modifier = Modifier,
-    onClick: () -> Unit
 ) {
     Box(
         modifier = modifier
             .fillMaxWidth()
             .height(111.dp)
             .clip(RoundedCornerShape(16.dp))
-            .background(AppTheme.colors.errorVariant),
+            .background(AppTheme.colors.errorVariant)
+            .padding(end = 10.dp),
         contentAlignment = Alignment.CenterEnd
     ) {
-
-        IconButton(
-            onClick = { onClick() },
-        ) {
-
-            Icon(
-                painter = painterResource(R.drawable.delete_icon),
-                contentDescription = "delete icon",
-                modifier = Modifier.size(32.dp),
-                tint = AppTheme.colors.error
-            )
-        }
-
+        Icon(
+            painter = painterResource(R.drawable.delete_icon),
+            contentDescription = "delete icon",
+            modifier = Modifier.size(32.dp),
+            tint = AppTheme.colors.error
+        )
     }
 
 
@@ -73,6 +67,13 @@ fun DeleteTaskScroll(
     val swipeableState = rememberSwipeableState(initialValue = 0)
     val anchors = mapOf(0f to 0, -maxOffset to 1)
 
+    LaunchedEffect(swipeableState.currentValue) {
+        if (swipeableState.currentValue == 1) {
+            onDelete()
+            swipeableState.animateTo(0)
+        }
+    }
+
     CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
         Box(
             modifier = Modifier
@@ -86,11 +87,7 @@ fun DeleteTaskScroll(
                     resistance = null
                 )
         ) {
-            DeleteTaskBackground(
-                modifier = Modifier.matchParentSize(),
-                onClick = onDelete
-            )
-
+            DeleteTaskBackground(modifier = Modifier.matchParentSize())
             Box(
                 modifier = Modifier
                     .offset {
