41a8b17c3f389b3f2c5273f4ce37fe799cc18264
omer1998 <omerfaris11@gmail.com>
2025-06-25 17:59:29 +0300

    clean and fix imports

--- app/src/main/java/com/washingtondcsquad/tudee/presentation/features/categories/CategoriesScreen.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/features/categories/CategoriesScreen.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -29,9 +32,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.washingtondcsquad.tudee.R
 import com.washingtondcsquad.tudee.domain.entity.CategoryID
+import com.washingtondcsquad.tudee.domain.entity.ImageSource
 import com.washingtondcsquad.tudee.presentation.components.CategoryCard
 import com.washingtondcsquad.tudee.presentation.design.AppTheme
-import com.washingtondcsquad.tudee.domain.entity.ImageSource
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -108,7 +111,6 @@ fun CategoriesContent(
             .background(AppTheme.colors.surfaceHigh)
             .windowInsetsPadding(WindowInsets.systemBars)
             .fillMaxSize()
-            .background(AppTheme.colors.surfaceHigh)
     ) {
         Column(
             modifier = Modifier.fillMaxSize()
