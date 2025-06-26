a59517c5a867bd4193a8049301bd09a00c4f7631
omer1998 <omerfaris11@gmail.com>
2025-06-27 00:59:55 +0300

    fix the size of category image

--- app/src/main/java/com/washingtondcsquad/tudee/presentation/components/LoadImage.kt
+++ app/src/main/java/com/washingtondcsquad/tudee/presentation/components/LoadImage.kt
@@ -1,14 +1,21 @@
 package com.washingtondcsquad.tudee.presentation.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.washingtondcsquad.tudee.domain.entity.ImageSource
+import com.washingtondcsquad.tudee.presentation.design.AppTheme
 
 @Composable
 fun LoadImage(imageSource: ImageSource, modifier: Modifier = Modifier) {
@@ -22,7 +29,14 @@ fun LoadImage(imageSource: ImageSource, modifier: Modifier = Modifier) {
                     .crossfade(true)
                     .build(),
                 contentDescription = "",
-                modifier = modifier,
+                modifier = Modifier
+                    .background(
+                        color = AppTheme.colors.surfaceHigh,
+                        shape = CircleShape
+                    )
+                    .padding(18.dp)
+                    .size(44.dp)
+                    .clip(CircleShape),
                 contentScale = ContentScale.Crop
             )
 
